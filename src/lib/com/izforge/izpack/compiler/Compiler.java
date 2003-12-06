041104454ba09ec5a62ba65112142ce1f7b6ca5e
jpz <jpz@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2003-12-06 16:34:47 +0000

    3.4.0 release
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@559 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/compiler/Compiler.java
+++ src/lib/com/izforge/izpack/compiler/Compiler.java
@@ -80,7 +80,7 @@ public class Compiler extends Thread
   public final static String VERSION = "1.0";
 
   /**  The IzPack version. */
-  public final static String IZPACK_VERSION = "3.3.0 (build 2003.11.15)";
+  public final static String IZPACK_VERSION = "3.4.0 (build 2003.12.06)";
 
   /**  Standard installer. */
   public final static String STANDARD = "standard";
