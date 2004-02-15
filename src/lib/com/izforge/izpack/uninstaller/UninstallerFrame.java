6b155fe5b877e3561f59603f882f6f81673c39a6
jponge <jponge@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2004-02-15 14:24:51 +0000

    Uninstaller: proper use of the ButtonFactory.
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@571 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/uninstaller/UninstallerFrame.java
+++ src/lib/com/izforge/izpack/uninstaller/UninstallerFrame.java
@@ -158,6 +158,10 @@ public class UninstallerFrame extends JFrame
       new KeyAdapter()
       {
       });
+      
+    // We set-up the buttons factory
+    ButtonFactory.useButtonIcons();
+    ButtonFactory.useHighlightButtons();
 
     // We put our components
 
