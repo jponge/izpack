b62095ff6d957ccb60f6fca67447f2c2cb2bbf40
jponge <jponge@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2004-03-15 17:10:26 +0000

    Preparing for 3.5.1.
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/branches/branch-3-5@614 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/compiler/Compiler.java
+++ src/lib/com/izforge/izpack/compiler/Compiler.java
@@ -85,7 +85,7 @@ public class Compiler extends Thread
   public final static String VERSION = "1.0";
 
   /**  The IzPack version. */
-  public final static String IZPACK_VERSION = "3.5.0 (build 2004.02.26)";
+  public final static String IZPACK_VERSION = "3.5.1 (build 2004.03.16)";
 
   /**  Standard installer. */
   public final static String STANDARD = "standard";
