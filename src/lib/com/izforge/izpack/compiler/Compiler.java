37414788fa92148c5d99b659a66289496a920f18
tschwarze <tschwarze@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2004-07-01 14:09:25 +0000

    fix bug: LnF-JARs are IzPack resources, not Project resources (makes standalone compiler work again) fall back to IzPack resources if Project resources not found
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/branches/branch-3-6@777 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/compiler/Compiler.java
+++ src/lib/com/izforge/izpack/compiler/Compiler.java
@@ -284,7 +284,7 @@ public class Compiler extends Thread
         if (lafJarName == null)
           parseError(gp, "Unrecognized Look and Feel: " + lafName);
 
-        URL lafJarURL = findProjectResource("lib/" + lafJarName,
+        URL lafJarURL = findIzPackResource("lib/" + lafJarName,
                                             "Look and Feel Jar file", gp);
         packager.addJarContent(lafJarURL);
       }
@@ -926,8 +926,7 @@ public class Compiler extends Thread
   /**
    * Look for a project specified resources, which, if not absolute, are sought
    * relative to the projects basedir. The path should use '/' as the
-   * fileSeparator. If the resource is not found, a CompilerException is thrown
-   * indicating fault in the parent element.
+   * fileSeparator. If the resource is not found, IzPack resources are tried next.
    *
    * @param path the relative path (using '/' as separator) to the resource.
    * @param desc the description of the resource used to report errors
@@ -943,8 +942,8 @@ public class Compiler extends Thread
     if (! resource.isAbsolute())
       resource = new File(basedir, path);
 
-    if (! resource.exists()) // fatal
-      parseError(parent, path + " not found");
+    if (! resource.exists()) // try IzPack resources as fallback
+      return findIzPackResource(path, desc, parent);
 
     try
     {
