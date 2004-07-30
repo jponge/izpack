1251b74178397889b150d0c5b306b6c544bc53ae
bartzkau <bartzkau@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2004-07-30 07:54:37 +0000

    Custom action stuff; uninstaller listener interface as simple implementation.
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@812 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/lib/com/izforge/izpack/uninstaller/SimpleUninstallerListener.java
@@ -0,0 +1,126 @@
+/*
+ *  $Id$
+ *  IzPack
+ *  Copyright (C) 2004 Klaus Bartz
+ *
+ *  File :               SimpleUninstallerListener.java
+ *  Description :        Simple custom action listener implementation 
+ *                       for uninstall time.
+ *  Author's email :     klaus.bartz@coi.de
+ *  Author's Website :   http://www.coi.de/
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.izforge.izpack.uninstaller;
+
+import java.io.File;
+import java.util.List;
+
+import com.izforge.izpack.util.AbstractUIProgressHandler;
+
+/**
+ * <p>This class implements all methods of interface
+ * UninstallerListener, but do not do enything.
+ * It can be used as base class to save implementation
+ * of unneeded methods.</p>
+ *
+ * @author  Klaus Bartz
+ *
+ */
+public class SimpleUninstallerListener implements UninstallerListener
+{
+
+  /**
+   * 
+   */
+  public SimpleUninstallerListener()
+  {
+    super();
+  }
+
+  /* (non-Javadoc)
+   * @see com.izforge.izpack.uninstaller.UninstallerListener#beforeExecutes(java.util.List, com.izforge.izpack.util.AbstractUIProgressHandler)
+   */
+  public void beforeExecutables(
+    List executables,
+    AbstractUIProgressHandler handler)
+    throws Exception
+  {
+    // Do nothing
+    ;
+  }
+
+  /* (non-Javadoc)
+   * @see com.izforge.izpack.uninstaller.UninstallerListener#afterExecutes(java.util.List, com.izforge.izpack.util.AbstractUIProgressHandler)
+   */
+  public void afterExecutables(
+    List executables,
+    AbstractUIProgressHandler handler)
+    throws Exception
+  {
+    // Do nothing
+    ;
+  }
+
+  /* (non-Javadoc)
+   * @see com.izforge.izpack.uninstaller.UninstallerListener#beforeDeletion(java.util.List, com.izforge.izpack.util.AbstractUIProgressHandler)
+   */
+  public void beforeDeletion(List files, AbstractUIProgressHandler handler)
+    throws Exception
+  {
+    // Do nothing
+    ;
+  }
+
+  /* (non-Javadoc)
+   * @see com.izforge.izpack.uninstaller.UninstallerListener#beforeDelete(java.io.File, com.izforge.izpack.util.AbstractUIProgressHandler)
+   */
+  public void beforeDelete(File file, AbstractUIProgressHandler handler)
+    throws Exception
+  {
+    // Do nothing
+    ;
+  }
+
+  /* (non-Javadoc)
+   * @see com.izforge.izpack.uninstaller.UninstallerListener#afterDelete(java.io.File, com.izforge.izpack.util.AbstractUIProgressHandler)
+   */
+  public void afterDelete(File file, AbstractUIProgressHandler handler)
+    throws Exception
+  {
+    // Do nothing
+    ;
+  }
+
+  /* (non-Javadoc)
+   * @see com.izforge.izpack.uninstaller.UninstallerListener#afterDeletion(java.util.List, com.izforge.izpack.util.AbstractUIProgressHandler)
+   */
+  public void afterDeletion(List files, AbstractUIProgressHandler handler)
+      throws Exception
+  {
+    // Do nothing
+    ;
+  }
+
+  /* (non-Javadoc)
+   * @see com.izforge.izpack.uninstaller.UninstallerListener#isFileListener()
+   */
+  public boolean isFileListener()
+  {
+    return false;
+  }
+
+}
