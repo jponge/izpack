09bbe38448bf09782a1227ff20554000995e3c9f
eppelman <eppelman@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2005-05-09 14:21:35 +0000

    added is kde installed - test
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1196 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/lib/com/izforge/izpack/util/UnixHelper.java
@@ -0,0 +1,64 @@
+/*
+ * IzPack - Copyright 2001-2005 Julien Ponge, All Rights Reserved.
+ * 
+ * http://www.izforge.com/izpack/ http://developer.berlios.de/projects/izpack/
+ * 
+ * Copyright 2004 Marc Eppelmann
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.izforge.izpack.util;
+
+/**
+ * Help Methods for unix-systems and derived.
+ * 
+ * @author marc.eppelmann&#064;reddot.de
+ */
+public class UnixHelper
+{
+
+    /**
+     * Testmain
+     * 
+     * @param args commandline args
+     */
+    public static void main(String[] args)
+    {
+        kdeIsInstalled();
+    }
+
+    /**
+     * Test if KDE is installed.
+     * This is done by $>/usr/bin/env konqueror --version
+     * 
+     * This assumes that the konqueror as a main-app of kde is already installed. 
+     * 
+     * If this returns with 0 konqeror and resp. kde means to be installed, 
+     * 
+     * @return true if kde is installed otherwise false.
+     */
+    public static boolean kdeIsInstalled()
+    {
+        boolean result = false;
+
+        FileExecutor fe = new FileExecutor();
+
+        String[] execOut = new String[2];
+
+        int execResult = fe.executeCommand(
+                new String[] { "/usr/bin/env", "konqueror", "--version"}, execOut);
+
+        System.out.println(execOut[0] + "\n\n");
+        System.out.println(execOut[1]);
+
+        return execResult == 0;
+    }
+}
