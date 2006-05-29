90cfd0ff3b3c6206f19655f4b419c880a28ac823
miraodb <miraodb@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2006-05-29 08:44:28 +0000

    Fixed The BackSlash into Slash. FM
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1430 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/createPWD.sh
+++ src/lib/createPWD.sh
@@ -8,7 +8,7 @@ echo "Enter the name of the jar you want to create:"
 read fname
 
 if [ -n "$fname" ]; then 	# -n tests to see if the argument is non empty
-	jar cf $fname .\com\izforge\izpack\sample\*.class
+	jar cf $fname ./com/izforge/izpack/sample/*.class
 	echo "Success: jar $fname has been created"
 else
 	echo "No file name entered !"
