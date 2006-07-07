b0a9d95e20fd7dbb1ff6cb73e6b9626a0efc4aa0
aikebah <aikebah@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2006-07-07 22:04:32 +0000

    added JavaDoc comments
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1483 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/tests/com/izforge/izpack/Bin_Langpacks_InstallerTest.java
+++ src/tests/com/izforge/izpack/Bin_Langpacks_InstallerTest.java
@@ -41,11 +41,24 @@ public class Bin_Langpacks_InstallerTest extends TestCase
     private static LocaleDatabase reference;
     private LocaleDatabase check;
 
+    /**
+     * Creates a new 'test all' installer langpack testcase
+     * 
+     * @throws Exception Forwarded Exception from LocaleDatabase constructor or FileNotFoundException on allocation of the reference languagepack
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     */
     public Bin_Langpacks_InstallerTest() throws Exception
     {
         this("");
     }
     
+    /**
+     * Creates a new 'single testmethod' installer langpack testcase
+     * 
+     * @throws Exception Forwarded Exception from LocaleDatabase constructor or FileNotFoundException on allocation of the reference languagepack
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see junit.framework.TestCase#TestCase(java.lang.String)
+     */
     public Bin_Langpacks_InstallerTest(String arg0) throws Exception
     {
         super(arg0);
@@ -53,96 +66,309 @@ public class Bin_Langpacks_InstallerTest extends TestCase
     }
     
     private void checkLangpack(String langpack) throws Exception{
-        check = new LocaleDatabase(new FileInputStream(basePath + langpack));
+        this.check = new LocaleDatabase(new FileInputStream(basePath + langpack));
         // all keys in the English langpack should be present in the foreign langpack
         for (Iterator i = reference.keySet().iterator();i.hasNext();) {
             // Locale Database uses the id strings as keys
             String id = (String) i.next();
-            assertTrue("Missing translation for id:"+id,check.containsKey(id));
+            assertTrue("Missing translation for id:"+id,this.check.containsKey(id));
         }
         // there should be no keys in the foreign langpack which don't exist in the 
         // english langpack
-        for (Iterator i = check.keySet().iterator();i.hasNext();) {
+        for (Iterator i = this.check.keySet().iterator();i.hasNext();) {
             // LocaleDatabase uses the id strings as keys
             String id = (String) i.next();
             assertTrue("Superfluous translation for id:"+id,reference.containsKey(id));
         }
     }
+    /**
+     * Checks the Catalan language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testCat() throws Exception{
         this.checkLangpack("cat.xml");
     }
-    public void testChn() throws Exception{
+    
+    /**
+     * Checks the Chinese language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
+public void testChn() throws Exception{
         this.checkLangpack("chn.xml");
     }
+
+/**
+ * Checks the Czech language pack for missing / superfluous translations
+ * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+ * 
+ * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+ * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+ * @see java.util.TreeMap#containsKey(java.lang.Object)
+ */
     public void testCze() throws Exception{
         this.checkLangpack("cze.xml");
     }
-    public void testDan() throws Exception{
+    
+    /**
+     * Checks the Danish language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
+public void testDan() throws Exception{
         this.checkLangpack("dan.xml");
     }
-    public void testDeu() throws Exception{
+/**
+ * Checks the German language pack for missing / superfluous translations
+ * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+ * 
+ * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+ * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+ * @see java.util.TreeMap#containsKey(java.lang.Object)
+ */
+public void testDeu() throws Exception{
         this.checkLangpack("deu.xml");
     }
-    public void testEll() throws Exception{
+/**
+ * Checks the Modern Greek language pack for missing / superfluous translations
+ * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+ * 
+ * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+ * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+ * @see java.util.TreeMap#containsKey(java.lang.Object)
+ */
+public void testEll() throws Exception{
         this.checkLangpack("ell.xml");
     }
-    public void testEng() throws Exception{
+
+/**
+ * Checks the English language pack for missing / superfluous translations<br />
+ * <em>This test should always succeed, since the english langpack is the reference pack)</em>
+ * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+ * 
+ * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+ * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+ * @see java.util.TreeMap#containsKey(java.lang.Object)
+ */
+public void testEng() throws Exception{
         this.checkLangpack("eng.xml");
     }
+/**
+ * Checks the Finnish language pack for missing / superfluous translations
+ * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+ * 
+ * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+ * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+ * @see java.util.TreeMap#containsKey(java.lang.Object)
+ */
     public void testFin() throws Exception{
         this.checkLangpack("fin.xml");
     }
+    /**
+     * Checks the French language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testFra() throws Exception{
         this.checkLangpack("fra.xml");
     }
+    /**
+     * Checks the Hungarian language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testHun() throws Exception{
         this.checkLangpack("hun.xml");
     }
+    /**
+     * Checks the Italian language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testIta() throws Exception{
         this.checkLangpack("ita.xml");
     }
+    /**
+     * Checks the Japanese language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testJpn() throws Exception{
         this.checkLangpack("jpn.xml");
     }
+    /**
+     * Checks the Korean language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testKor() throws Exception{
         this.checkLangpack("kor.xml");
     }
+    /**
+     * Checks the Malaysian language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testMys() throws Exception{
         this.checkLangpack("mys.xml");
     }
+    /**
+     * Checks the Dutch language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testNed() throws Exception{
         this.checkLangpack("ned.xml");
     }
+    /**
+     * Checks the Norwegian language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testNor() throws Exception{
         this.checkLangpack("nor.xml");
     }
+    /**
+     * Checks the Polish language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testPol() throws Exception{
         this.checkLangpack("pol.xml");
     }
+    /**
+     * Checks the Portugese language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testPor() throws Exception{
         this.checkLangpack("por.xml");
     }
+    /**
+     * Checks the Romanian language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testRom() throws Exception{
         this.checkLangpack("rom.xml");
     }
+    /**
+     * Checks the Russian language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testRus() throws Exception{
         this.checkLangpack("rus.xml");
     }
+    /**
+     * Checks the Serbia/Montenegro language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testScg() throws Exception{
         this.checkLangpack("scg.xml");
     }
+    /**
+     * Checks the Spanish language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testSpa() throws Exception{
         this.checkLangpack("spa.xml");
     }
+    /**
+     * Checks the Slovak language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testSvk() throws Exception{
         this.checkLangpack("svk.xml");
     }
-    public void testSwe() throws Exception{
+    /**
+     * Checks the Swedish language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
+public void testSwe() throws Exception{
         this.checkLangpack("swe.xml");
     }
+/**
+ * Checks the Turkish language pack for missing / superfluous translations
+ * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+ * 
+ * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+ * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+ * @see java.util.TreeMap#containsKey(java.lang.Object)
+ */
     public void testTur() throws Exception{
         this.checkLangpack("tur.xml");
     }
+    /**
+     * Checks the Ukranian language pack for missing / superfluous translations
+     * @throws Exception Forwarded Exception for the LocaleDatabase contructor, FileInputStream constructor or TreeMap containsKey method
+     * 
+     * @see com.izforge.izpack.LocaleDatabase#LocaleDatabase(java.io.InputStream)
+     * @see java.io.FileInputStream#FileInputStream(java.lang.String)
+     * @see java.util.TreeMap#containsKey(java.lang.Object)
+     */
     public void testUkr() throws Exception{
         this.checkLangpack("ukr.xml");
     }
