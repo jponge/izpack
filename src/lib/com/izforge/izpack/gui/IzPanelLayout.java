a41d10f8d4b44465c9a7797dc48f97da4a268b56
bartzkau <bartzkau@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2006-08-02 15:13:18 +0000

    License header added...
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1511 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/gui/IzPanelLayout.java
+++ src/lib/com/izforge/izpack/gui/IzPanelLayout.java
@@ -1,3 +1,24 @@
+/*
+ * $Id:$
+ * IzPack - Copyright 2001-2006 Julien Ponge, All Rights Reserved.
+ * 
+ * http://www.izforge.com/izpack/
+ * http://developer.berlios.de/projects/izpack/
+ * 
+ * Copyright 2006 Klaus Bartz
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.izforge.izpack.gui;
 
 import java.awt.Component;
@@ -8,11 +29,8 @@ import java.awt.LayoutManager;
 import java.awt.LayoutManager2;
 import java.awt.Rectangle;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 import javax.swing.text.JTextComponent;
 
 import com.izforge.izpack.util.MultiLineLabel;
--- src/lib/com/izforge/izpack/installer/LayoutHelper.java
+++ src/lib/com/izforge/izpack/installer/LayoutHelper.java
@@ -1,19 +1,23 @@
 /*
- * $Id:$ IzPack - Copyright 2001-2006 Julien Ponge, All Rights Reserved.
+ * $Id:$
+ * IzPack - Copyright 2001-2006 Julien Ponge, All Rights Reserved.
  * 
- * http://www.izforge.com/izpack/ http://developer.berlios.de/projects/izpack/
+ * http://www.izforge.com/izpack/
+ * http://developer.berlios.de/projects/izpack/
  * 
  * Copyright 2006 Klaus Bartz
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.izforge.izpack.installer;
 
@@ -22,10 +26,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.LayoutManager2;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.SwingConstants;
 
 import com.izforge.izpack.gui.IzPanelConstraints;
 import com.izforge.izpack.gui.IzPanelLayout;
