f804e067cc584743510918681952ddff05ed0511
eppelman <eppelman@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2006-09-06 20:33:14 +0000

    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1568 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/lib/com/izforge/izpack/util/os/unix/UnixConstants.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.izforge.izpack.util.os.unix;
+
+
+/**
+ * @author marc.eppelmann
+ *
+ */
+public class UnixConstants
+{
+    /** etcPasswd = "/etc/passwd" */
+    public static String etcPasswd = "/etc/passwd";
+    
+    
+    /**
+     * 
+     */
+    public UnixConstants()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
