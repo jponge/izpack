f66a1f65afe70df73f82a261496b97697f08cedf
bartzkau <bartzkau@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2007-03-13 17:05:28 +0000

    Interface which should contain common used strings etc. for the new logging stuff.
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1783 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/lib/com/izforge/izpack/util/DebugConstants.java
@@ -0,0 +1,37 @@
+/*
+ * $Id:$
+ * IzPack - Copyright 2001-2007 Julien Ponge, All Rights Reserved.
+ *
+ * http://www.izforge.com/izpack/
+ * http://developer.berlios.de/projects/izpack/
+ *
+ * Copyright 2007 Klaus Bartz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.izforge.izpack.util;
+
+
+/**
+ * This interface contains common used templates and channel names for debug messages 
+ * using class 
+ * {@link com.izforge.izpack.util.Log <code>LogMessage</code>}
+ * 
+ * @author Klaus Bartz
+ *
+ */
+public interface DebugConstants
+{
+    /** Channel name for debug message about panel tracing. */
+    public final static String PANEL_TRACE = "PanelTrace";
+}
