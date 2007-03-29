b203c9d9d7b693d40d52156e6045bc1f94c414d7
jponge <jponge@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2007-03-29 13:24:02 +0000

    3.10.1
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/branches/3.10@1797 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/compiler/Compiler.java
+++ src/lib/com/izforge/izpack/compiler/Compiler.java
@@ -1,21 +1,21 @@
 /*
  * $Id$
  * IzPack - Copyright 2001-2007 Julien Ponge, All Rights Reserved.
- * 
+ *
  * http://www.izforge.com/izpack/
  * http://developer.berlios.de/projects/izpack/
- * 
+ *
  * Copyright 2001 Johannes Lehtinen
  * Copyright 2002 Paul Wilkinson
  * Copyright 2004 Gaganis Giorgos
  *
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -54,9 +54,9 @@ import com.izforge.izpack.util.VariableSubstitutor;
  * configured using the object representations of the install.xml
  * configuration. The install.xml configuration is now handled by the
  * CompilerConfig class.
- * 
+ *
  * @see CompilerConfig
- * 
+ *
  * @author Julien Ponge
  * @author Tino Schwarze
  * @author Chadwick McHenry
@@ -64,7 +64,7 @@ import com.izforge.izpack.util.VariableSubstitutor;
 public class Compiler extends Thread
 {
     /** The IzPack version. */
-    public final static String IZPACK_VERSION = "3.10.0";
+    public final static String IZPACK_VERSION = "3.10.1";
 
     /** The IzPack home directory. */
     public static String IZPACK_HOME = ".";
@@ -89,7 +89,7 @@ public class Compiler extends Thread
 
     /** Replaces the properties in the install.xml file prior to compiling */
     private VariableSubstitutor propertySubstitutor;
-    
+
     private String compr_format;
     private int compr_level;
     private PackagerListener packagerlistener;
@@ -105,7 +105,7 @@ public class Compiler extends Thread
 
     /**
      * The constructor.
-     * 
+     *
      * @param basedir The base directory.
      * @param kind The installer kind.
      * @param output The installer filename.
@@ -118,7 +118,7 @@ public class Compiler extends Thread
 
     /**
      * The constructor.
-     * 
+     *
      * @param basedir The base directory.
      * @param kind The installer kind.
      * @param output The installer filename.
@@ -132,7 +132,7 @@ public class Compiler extends Thread
 
     /**
      * The constructor.
-     * 
+     *
      * @param basedir The base directory.
      * @param kind The installer kind.
      * @param output The installer filename.
@@ -140,7 +140,7 @@ public class Compiler extends Thread
      * @param compr_level Compression level to be used if supported.
      * @throws CompilerException
      */
-    public Compiler(String basedir, String kind, String output, 
+    public Compiler(String basedir, String kind, String output,
             String compr_format, int compr_level) throws CompilerException
     {
         // Default initialisation
@@ -155,7 +155,7 @@ public class Compiler extends Thread
         // add izpack built in property
         setProperty("izpack.version", IZPACK_VERSION);
         setProperty("basedir", basedir);
-        
+
         this.compr_format = compr_format;
         this.compr_level = compr_level;
     }
@@ -182,7 +182,7 @@ public class Compiler extends Thread
             throw new CompilerException("Error loading packager class: " +  classname);
         }
     }
-    
+
     /**
      * Returns the packager listener.
      * @return the packager listener
@@ -193,7 +193,7 @@ public class Compiler extends Thread
     }
     /**
      * Sets the packager listener.
-     * 
+     *
      * @param listener The listener.
      */
     public void setPackagerListener(PackagerListener listener)
@@ -256,7 +256,7 @@ public class Compiler extends Thread
 
     /**
      * Compiles the installation.
-     * 
+     *
      * @exception Exception Description of the Exception
      */
     public void createInstaller() throws Exception
@@ -274,10 +274,10 @@ public class Compiler extends Thread
                 if( decoderClassNames[i] != null && decoderClassNames[i].length > 0)
                     addJarContent(compressorURL, Arrays.asList(decoderClassNames[i]));
             }
-            
-            
+
+
         }
-  
+
         // We ask the packager to create the installer
         packager.createInstaller(new File(output));
         this.compileFailed = false;
@@ -339,7 +339,7 @@ public class Compiler extends Thread
 
     /**
      * Get the value of a property currerntly known to izpack.
-     * 
+     *
      * @param name the name of the property
      * @return the value of the property, or null
      */
@@ -350,7 +350,7 @@ public class Compiler extends Thread
 
     /**
      * Add a name value pair to the project property set. Overwriting any existing value except system properties.
-     * 
+     *
      * @param name the name of the property
      * @param value the value to set
      * @return an indicator if the name value pair was added.
@@ -367,7 +367,7 @@ public class Compiler extends Thread
     /**
      * Add a name value pair to the project property set. It is <i>not</i> replaced it is already
      * in the set of properties.
-     * 
+     *
      * @param name the name of the property
      * @param value the value to set
      * @return true if the property was not already set
@@ -385,7 +385,7 @@ public class Compiler extends Thread
 
     /**
      * Add jar content to the installation.
-     * @param content 
+     * @param content
      */
     public void addJarContent(URL content)
     {
@@ -396,7 +396,7 @@ public class Compiler extends Thread
      * Adds a jar file content to the installer. Package structure is maintained. Need mechanism to
      * copy over signed entry information. If the given file list is null the hole contents of the
      * jar file will be copied else only the listed.
-     * 
+     *
      * @param content The url of the jar to add to the installer. We use a URL so the jar may be
      * nested within another.
      * @param files to be copied
@@ -408,7 +408,7 @@ public class Compiler extends Thread
 
     /**
      * Add a custom jar to the installation.
-     * 
+     *
      * @param ca
      * @param url
      */
@@ -459,7 +459,7 @@ public class Compiler extends Thread
      */
     public void addPanelJar(Panel panel, URL url)
     {
-        packager.addPanelJar(panel, url);        
+        packager.addPanelJar(panel, url);
     }
     /**
      * Add a resource to the installation.
@@ -475,13 +475,13 @@ public class Compiler extends Thread
      * Checks whether the dependencies stated in the configuration file are correct. Specifically it
      * checks that no pack point to a non existent pack and also that there are no circular
      * dependencies in the packs.
-     * @throws CompilerException 
+     * @throws CompilerException
      */
     public void checkDependencies() throws CompilerException
     {
         checkDependencies(packager.getPacksList());
     }
-    
+
     /**
      * Checks whether the excluded packs exist. (simply calles the other function)
      * @throws CompilerException
@@ -490,11 +490,11 @@ public class Compiler extends Thread
     {
        checkExcludes(packager.getPacksList());
     }
-    
+
     /**
      * This checks if there are more than one preselected packs per excludeGroup.
      * @param packs list of packs which should be checked
-     * @throws CompilerException 
+     * @throws CompilerException
      */
     public void checkExcludes(List packs) throws CompilerException
     {
@@ -520,7 +520,7 @@ public class Compiler extends Thread
                     }
                 }
             }
-            
+
         }
     }
     /**
@@ -528,7 +528,7 @@ public class Compiler extends Thread
      * checks that no pack point to a non existent pack and also that there are no circular
      * dependencies in the packs.
      * @param packs - List<Pack> representing the packs in the installation
-     * @throws CompilerException 
+     * @throws CompilerException
      */
     public void checkDependencies(List packs) throws CompilerException
     {
@@ -551,7 +551,7 @@ public class Compiler extends Thread
      * We use the dfs graph search algorithm to check whether the graph is acyclic as described in:
      * Thomas H. Cormen, Charles Leiserson, Ronald Rivest and Clifford Stein. Introduction to
      * algorithms 2nd Edition 540-549,MIT Press, 2001
-     * 
+     *
      * @param packs The graph
      * @param names The name map
      * @return -2 if back edges exist, else 0
@@ -641,11 +641,11 @@ public class Compiler extends Thread
      * not be absolute. The path must use '/' as the fileSeparator (it's used to access the jar
      * file). If the resource is not found, a CompilerException is thrown indicating fault in the
      * parent element.
-     * 
+     *
      * @param path the relative path (using '/' as separator) to the resource.
      * @param desc the description of the resource used to report errors
      * @return a URL to the resource.
-     * @throws CompilerException 
+     * @throws CompilerException
      */
     public URL findIzPackResource(String path, String desc)
             throws CompilerException
@@ -675,9 +675,9 @@ public class Compiler extends Thread
     /**
      * Create parse error with consistent messages. Includes file name. For use When parent is
      * unknown.
-     * 
+     *
      * @param message Brief message explaining error
-     * @throws CompilerException 
+     * @throws CompilerException
      */
     public void parseError(String message) throws CompilerException
     {
@@ -687,10 +687,10 @@ public class Compiler extends Thread
     /**
      * Create parse error with consistent messages. Includes file name. For use When parent is
      * unknown.
-     * 
+     *
      * @param message Brief message explaining error
      * @param how throwable which was catched
-     * @throws CompilerException 
+     * @throws CompilerException
      */
     public void parseError(String message, Throwable how) throws CompilerException
     {
@@ -701,7 +701,7 @@ public class Compiler extends Thread
     /**
      * The main method if the compiler is invoked by a command-line call.
      * This simply calls the CompilerConfig.main method.
-     * 
+     *
      * @param args The arguments passed on the command-line.
      */
     public static void main(String[] args)
@@ -717,7 +717,7 @@ public class Compiler extends Thread
      * posible, the listeners will be validated. Listener declaration is a fragmention in
      * install.xml like : &lt;listeners&gt; &lt;listener compiler="PermissionCompilerListener"
      * installer="PermissionInstallerListener"/1gt; &lt;/listeners&gt;
-     * 
+     *
      * @param type The listener type.
      * @param className The class name.
      * @param jarPath The jar path.
@@ -737,7 +737,7 @@ public class Compiler extends Thread
     /**
      * Returns a list which contains the pathes of all files which are included in the given url.
      * This method expects as the url param a jar.
-     * 
+     *
      * @param url url of the jar file
      * @return full qualified paths of the contained files
      * @throws Exception
@@ -760,7 +760,7 @@ public class Compiler extends Thread
     /**
      * Returns the qualified class name for the given class. This method expects as the url param a
      * jar file which contains the given class. It scans the zip entries of the jar file.
-     * 
+     *
      * @param url url of the jar file which contains the class
      * @param className short name of the class for which the full name should be resolved
      * @return full qualified class name
@@ -800,7 +800,7 @@ public class Compiler extends Thread
 
     /**
      * Used to handle the packager messages in the command-line mode.
-     * 
+     *
      * @author julien created October 26, 2002
      */
     static class CmdlinePackagerListener implements PackagerListener
@@ -808,7 +808,7 @@ public class Compiler extends Thread
 
         /**
          * Print a message to the console at default priority (MSG_INFO).
-         * 
+         *
          * @param info The information.
          */
         public void packagerMsg(String info)
@@ -818,7 +818,7 @@ public class Compiler extends Thread
 
         /**
          * Print a message to the console at the specified priority.
-         * 
+         *
          * @param info The information.
          * @param priority priority to be used for the message prefix
          */
