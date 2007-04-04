c76e5c77f1e13b9b9b52858b0b46e3706ff5c69e
aikebah <aikebah@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2007-04-04 21:27:06 +0000

    Fix NPE in getI18NPackName and corrected case in Dutch langpack
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/branches/3.10@1809 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/panels/PacksPanelBase.java
+++ src/lib/com/izforge/izpack/panels/PacksPanelBase.java
@@ -375,7 +375,7 @@ public abstract class PacksPanelBase extends IzPanel implements PacksPanelInterf
         {
             packName = langpack.getString(key);
         }
-        if ("".equals(packName) || key.equals(packName) || pack.id == null)
+        if ("".equals(packName) || key == null || key.equals(packName) )
         {
             packName = pack.name;
         }
