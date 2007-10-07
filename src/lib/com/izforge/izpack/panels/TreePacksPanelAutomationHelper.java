d7581e730ca6a3fba59ad8bc3df58bc0b1bdeadb
vralev <vralev@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2007-10-07 22:12:26 +0000

    Automated install helper for TreePacksPanel
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1878 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/lib/com/izforge/izpack/panels/TreePacksPanelAutomationHelper.java
@@ -0,0 +1,12 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package com.izforge.izpack.panels;
+
+public class TreePacksPanelAutomationHelper extends PacksPanelAutomationHelper
+{
+
+}
