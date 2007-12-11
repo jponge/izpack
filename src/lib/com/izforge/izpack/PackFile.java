1d555542d5c9d26407aa8083cd2b04a657e16076
jponge <jponge@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2007-12-11 22:12:27 +0000

    What is this GPL notice still doing here???
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@1946 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/PackFile.java
+++ src/lib/com/izforge/izpack/PackFile.java
@@ -19,30 +19,6 @@
  * limitations under the License.
  */
 
-/*
- *  $Id$
- *  IzPack
- *  Copyright (C) 2001 Johannes Lehtinen
- *
- *  File :               Pack.java
- *  Description :        Contains informations about a pack file.
- *  Author's email :     johannes.lehtinen@iki.fi
- *  Author's Website :   http://www.iki.fi/jle/
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
 package com.izforge.izpack;
 
 import java.io.File;
