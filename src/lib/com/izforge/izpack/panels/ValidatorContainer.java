df3e2fc90f85829fbe35a60f2ab2877f7f962e39
jgordon <jgordon@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2008-01-24 19:25:01 +0000

    Addition to support multiple validators.
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@2005 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/lib/com/izforge/izpack/panels/ValidatorContainer.java
@@ -0,0 +1,60 @@
+package com.izforge.izpack.panels;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Jeff Gordon
+ */
+public class ValidatorContainer {
+  private Validator validator = null;
+  private String message;
+  private boolean hasParams = false;
+  private Map validatorParams = null;
+
+  public ValidatorContainer(String validator, String message, Map validatorParams) {
+    try {
+      this.validator = (Validator) Class.forName(validator).newInstance();
+      this.message = message;
+      this.validatorParams = validatorParams;
+      if (validatorParams != null) {
+        if (validatorParams.size() > 0) {
+          hasParams = true;
+        }
+      }
+    } catch (Throwable e) {
+      System.out.println("ValidatorContainer Constructor Failed: "+e);
+      this.validator = null;
+      this.message = null;
+      hasParams = false;
+      validatorParams = null;
+    }
+  }
+
+  /**
+   * @return true if this instance has any parameters to pass to the Validator instance.
+   */
+  public boolean hasParams() {
+    return hasParams;
+  }
+  
+  /**
+   * Returns the validator parameters, if any. The caller should check for the existence of
+   * validator parameters via the <code>hasParams()</code> method prior to invoking this method.
+   * 
+   * @return a java.util.Map containing the validator parameters.
+   */
+  public Map getValidatorParams() {
+    return validatorParams;
+  }
+
+  public Validator getValidator() {
+    return validator;
+  }
+  
+  public String getMessage() {
+    return message;
+  }
+  
+  
+}
