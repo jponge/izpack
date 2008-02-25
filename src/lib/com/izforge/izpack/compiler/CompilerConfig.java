jponge/izpack

commit 8adb16fda4f1ca5cb959b0db1705c70636ebd900

IZPACK-11 Option for writing installation information
optional writing of installation information

git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@2054 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

src/lib/com/izforge/izpack/compiler/CompilerConfig.java
@@ -39,6 +39,7 @@ import com.izforge.izpack.event.CompilerListener;
 import com.izforge.izpack.rules.Condition;
 import com.izforge.izpack.rules.RulesEngine;
 import com.izforge.izpack.util.Debug;
+import com.izforge.izpack.util.Log;
 import com.izforge.izpack.util.OsConstraint;
 import com.izforge.izpack.util.VariableSubstitutor;
 import net.n3.nanoxml.IXMLParser;
@@ -1521,6 +1522,11 @@ public class CompilerConfig extends Thread
         XMLElement slfPath = root.getFirstChildNamed("summarylogfilepath");
         if (slfPath != null) info.setSummaryLogFilePath(requireContent(slfPath));
 
+        XMLElement writeInstallInfo = root.getFirstChildNamed("writeinstallationinformation");
+        
+        String writeInstallInfoString = requireContent(writeInstallInfo);        
+        info.setWriteInstallationInformation(validateYesNo(writeInstallInfoString));
+        
         // look for an unpacker class
         String unpackerclass = compiler.getProperty("UNPACKER_CLASS");
         info.setUnpackerClassName(unpackerclass);
@@ -1972,6 +1978,22 @@ public class CompilerConfig extends Thread
             parseError(element, "<" + element.getName() + "> requires content");
         return content;
     }
+    
+    protected boolean validateYesNo(String value){
+        boolean result = false;
+        if ("yes".equalsIgnoreCase(value)){
+            result = true;
+        }
+        else if ("no".equalsIgnoreCase(value)){
+            result = false;
+        }
+        else {
+            Debug.trace("yes/no not found. trying true/false");
+            result = Boolean.valueOf(value).booleanValue();
+        }   
+        return result;
+    }
+    
 
     /**
      * Call getAttribute on an element, producing a meaningful error message if not present, or

src/lib/com/izforge/izpack/Info.java
@@ -70,6 +70,8 @@ public class Info implements Serializable
     private String packDecoderClassName = null;
 
     private String unpackerClassName = null;
+    
+    private boolean writeInstallationInformation = true;
 
     /** The constructor, deliberatly void. */
     public Info()
@@ -375,5 +377,17 @@ public class Info implements Serializable
     public void setUnpackerClassName(String unpackerClassName)
     {
         this.unpackerClassName = unpackerClassName;
+    }
+
+    
+    public boolean isWriteInstallationInformation()
+    {
+        return writeInstallationInformation;
+    }
+
+    
+    public void setWriteInstallationInformation(boolean writeInstallationInformation)
+    {
+        this.writeInstallationInformation = writeInstallationInformation;
     }
 }

src/lib/com/izforge/izpack/installer/UnpackerBase.java
@@ -814,8 +814,12 @@ public abstract class UnpackerBase implements IUnpacker
      * installation time.
      * @throws IOException 
      * @throws ClassNotFoundException 
-     */
+     */    
     public void writeInstallationInformation() throws IOException, ClassNotFoundException {
+        if (!idata.info.isWriteInstallationInformation()){
+            Debug.trace("skip writing installation information");
+            return;
+        }
         Debug.trace("writing installation information");
         String installdir = idata.getInstallPath();
         

commit c42ae88179d165c2d43bd1746db4d237e77c9284

Fixes a regression introduced in r2054 for IZPACK-11.
git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@2059 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

src/lib/com/izforge/izpack/compiler/CompilerConfig.java
@@ -1522,10 +1522,12 @@ public class CompilerConfig extends Thread
         XMLElement slfPath = root.getFirstChildNamed("summarylogfilepath");
         if (slfPath != null) info.setSummaryLogFilePath(requireContent(slfPath));
 
-        XMLElement writeInstallInfo = root.getFirstChildNamed("writeinstallationinformation");
-        
-        String writeInstallInfoString = requireContent(writeInstallInfo);        
-        info.setWriteInstallationInformation(validateYesNo(writeInstallInfoString));
+        XMLElement writeInstallInfo = root.getFirstChildNamed("writeinstallationinformation");        
+        if (writeInstallInfo != null)
+        {
+            String writeInstallInfoString = requireContent(writeInstallInfo);
+            info.setWriteInstallationInformation(validateYesNo(writeInstallInfoString));
+        }
         
         // look for an unpacker class
         String unpackerclass = compiler.getProperty("UNPACKER_CLASS");