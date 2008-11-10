6933a711e12682dc93467a6e7dd7884cf6c52cf8
jponge <jponge@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2008-11-10 15:31:11 +0000

    Towards 4.1.1...
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/branches/4.1@2377 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/compiler/Compiler.java
+++ src/lib/com/izforge/izpack/compiler/Compiler.java
@@ -56,7 +56,7 @@ public class Compiler extends Thread
     /**
      * The IzPack version.
      */
-    public final static String IZPACK_VERSION = "4.1.0";
+    public final static String IZPACK_VERSION = "4.1.1";
 
     /**
      * The IzPack home directory.
