2ebaea0270c3b56e3a63412b998c98391ff8cc0c
avoutila <avoutila@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2008-12-31 18:07:32 +0000

    Added test for testing method substitute(..) of the class com.izforge.izpack.util.VariableSubstitutor. (IZPACK-212)
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@2463 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/tests/com/izforge/izpack/installer/SubstitutorTest.java
@@ -0,0 +1,98 @@
+/*
+ * IzPack - Copyright 2001-2009 Julien Ponge, All Rights Reserved.
+ *
+ * http://izpack.org/
+ * http://izpack.codehaus.org/
+ *
+ * Copyright 2009 Ari Voutilainen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izforge.izpack.installer;
+
+
+import junit.framework.TestCase;
+import com.izforge.izpack.util.VariableSubstitutor;
+
+
+/**
+ * This will test some characters as UTF-8.
+ *
+ * @note    This test will create VariableSubstitutor object with null parameter!
+ *          There won't be Properties object available!
+ *
+ * @author  Ari Voutilainen, ari.voutilainen (at) iki.fi
+ */
+public class SubstitutorTest extends TestCase
+{
+    private final String umlautString ="ÄöäÖüÜ";
+    private final String cyrillicString ="Закончить";
+    private final String japanesString = "ƒCƒ“ƒXƒg[ƒ‹‚Ì’†Ž~";
+    
+    protected VariableSubstitutor subst = new VariableSubstitutor(null);
+    
+    /**
+     * Constructor for the test.
+     * 
+     * @param arg0  Parameter string for the constructor.
+     */
+    public SubstitutorTest(String arg0)
+    {
+        super(arg0);
+    }
+    
+    /** (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    /** (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+    
+    
+    /**
+     * Tests return value of substitute for string containing umlaut characters.
+     */
+    public void testUmlautString()
+    {
+        String returnStr = subst.substitute(umlautString, null);
+        assertEquals(umlautString, returnStr);
+    }
+
+    /**
+     * Tests return value of substitute for Cyrillic string.
+     */
+    public void testCyrillicString()
+    {
+        String returnStr = subst.substitute(cyrillicString, null);
+        assertEquals(cyrillicString, returnStr);
+    }
+
+    /**
+     * Tests return value of substitute for Japanese string.
+     */
+    public void testJapaneseString()
+    {
+        String returnStr = subst.substitute(japanesString, null);
+        assertEquals(japanesString, returnStr);
+    }
+}
