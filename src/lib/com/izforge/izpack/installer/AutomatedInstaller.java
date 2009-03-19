df9c384930e1531997f6ea476030f6689304308f
dreil <dreil@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2009-03-19 13:25:52 +0000

    IZPACK-345 corrected the initialization of panelactions
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@2669 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- src/lib/com/izforge/izpack/installer/AutomatedInstaller.java
+++ src/lib/com/izforge/izpack/installer/AutomatedInstaller.java
@@ -585,6 +585,7 @@ public class AutomatedInstaller extends InstallerBase
             {
                 PanelAction action = PanelActionFactory.createPanelAction(actionClassName);
                 action.initialize(panel.getPanelActionConfiguration(actionClassName));
+                actionList.add(action);
             }
         }
         return actionList;
