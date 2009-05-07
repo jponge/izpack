c3cb8fd176d263bd86ac7e8bfdedaa7f1448d428
dreil <dreil@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2009-05-07 12:29:09 +0000

    IZPACK-389 created a general ConsolePanelAutomationHelper and hand it over to PanelActions
    git-svn-id: https://svn.codehaus.org/izpack/izpack-src/trunk@2771 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ src/lib/com/izforge/izpack/installer/ConsolePanelAutomationHelper.java
@@ -0,0 +1,29 @@
+/*
+ * IzPack - Copyright 2001-2008 Julien Ponge, All Rights Reserved.
+ * 
+ * http://izpack.org/
+ * http://izpack.codehaus.org/
+ * 
+ * Copyright 2009 Dennis Reil, <izpack@reil-online.de>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izforge.izpack.installer;
+
+
+
+public class ConsolePanelAutomationHelper extends PanelAutomationHelper
+{
+
+}
