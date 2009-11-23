603f7f82660cc9647a6a9d83cba9ce109cec05ad
Anthonin Bonnefoy <bonnefoa@poste.isima.fr>
2009-11-23 07:40:24 +0100

    Dependencies correction and creation of test classes

---
+++ izpack-test/src/test/java/org/izforge/izpack/CompilationTest.java
@@ -0,0 +1,13 @@
+package org.izforge.izpack;
+
+import org.junit.Test;
+
+/**
+ * Test for an Izpack compilation
+ */
+public class CompilationTest {
+    @Test
+    public void testCompile() {
+
+    }
+}
---
+++ izpack-test/src/test/java/org/izforge/izpack/InstallationTest.java
@@ -0,0 +1,14 @@
+package org.izforge.izpack;
+
+import org.junit.Test;
+
+/**
+ * Test for an end-to-end installation
+ */
+public class InstallationTest {
+
+    @Test
+    public void testInstall() {
+
+    }
+}
