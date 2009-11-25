396dd3cfc2fb9a9f7450dd02526254a9d6ec9817
Anthonin Bonnefoy <bonnefoa@poste.isima.fr>
2009-11-25 22:22:21 +0100

    Launched created jar, binding fest next

--- izpack-test/src/test/java/org/izforge/izpack/InstallationTest.java
+++ izpack-test/src/test/java/org/izforge/izpack/InstallationTest.java
@@ -4,7 +4,7 @@ import org.hamcrest.core.Is;
 import org.junit.Test;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.lang.reflect.Method;
 import java.net.URL;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,12 +15,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class InstallationTest {
 
     @Test
-    public void testInstallSamples1() throws MalformedURLException {
+    public void testInstallSamples1() throws Exception {
         URL urls[] = {};
-        JarFileLoader cl = new JarFileLoader(urls);
+
+        JarFileLoader jarLoader = new JarFileLoader(urls);
         File file = new File(getClass().getClassLoader().getResource("samples1/out.jar").getFile());
         assertThat(file.exists(), Is.is(true));
-        cl.addFile(file.getAbsolutePath());
+        jarLoader.addFile(file.getAbsolutePath());
+        Class c = jarLoader.loadClass("com.izforge.izpack.installer.Installer");
+
+        Object o = c.newInstance();
+        Class[] argTypes = new Class[]{String[].class};
+        Method main = c.getDeclaredMethod("main", argTypes);
+        System.out.format("invoking %s.main()%n", c.getName());
+        String[] mainArgs = new String[0];
+        main.invoke(null, (Object) mainArgs);
+
+        Thread.sleep(1000);
+
+//        Installer.main(new String[]{""});
+
 //            cl.loadClass("org.gjt.mm.mysql.Driver");
 
     }
