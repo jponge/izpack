022495353804eb4eee35bc44025f111f6a8a884d
Anthonin Bonnefoy <bonnefoa@poste.isima.fr>
2009-11-30 18:47:10 +0100

    Modified xvfb path

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./xvfb-run -s "-screen 0 1280x1024x24" /opt/maven/apache-maven-2.2.0/bin/mvn clean install
+/usr/local/bin/xvfb-run -s "-screen 0 1280x1024x24" /opt/maven/apache-maven-2.2.0/bin/mvn clean install
