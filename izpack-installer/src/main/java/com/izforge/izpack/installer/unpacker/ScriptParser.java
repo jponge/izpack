9b546479eb30600b4cefebad1f61f61e51f64d45
Anthonin Bonnefoy <bonnefoa@poste.isima.fr>
2009-11-30 22:25:15 +0100

    Extracted constants from ScriptParser

--- izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/ScriptParser.java
+++ izpack-installer/src/main/java/com/izforge/izpack/installer/unpacker/ScriptParser.java
@@ -38,67 +38,6 @@ import java.util.Iterator;
 public class ScriptParser {
 
     /**
-     * The Java home path.
-     */
-    public final static String JAVA_HOME = "JAVA_HOME";
-
-    /**
-     * The ClassPath.
-     */
-    public final static String CLASS_PATH = "CLASS_PATH";
-
-    /**
-     * The user home path.
-     */
-    public final static String USER_HOME = "USER_HOME";
-
-    /**
-     * The user name.
-     */
-    public final static String USER_NAME = "USER_NAME";
-
-    /**
-     * The hostname.
-     */
-    public final static String HOST_NAME = "HOST_NAME";
-
-    /**
-     * The ip address.
-     */
-    public final static String IP_ADDRESS = "IP_ADDRESS";
-
-    /**
-     * The file separator character.
-     */
-    public final static String FILE_SEPARATOR = "FILE_SEPARATOR";
-
-    /**
-     * The application name.
-     */
-    public final static String APP_NAME = "APP_NAME";
-
-    /**
-     * The application URL.
-     */
-    public final static String APP_URL = "APP_URL";
-
-    /**
-     * The application version.
-     */
-    public final static String APP_VER = "APP_VER";
-
-    /**
-     * The language IS03 code.
-     */
-    public final static String ISO3_LANG = "ISO3_LANG";
-
-    /**
-     * The language code as _ll_CC like used with ResourceBoundle.
-     */
-    public final static String LOCALE = "LOCALE_IDENTIFIER";
-
-
-    /**
      * The files to parse.
      */
     private Collection<ParsableFile> files;
