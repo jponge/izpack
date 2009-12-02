5fe9cdf3cccb766bb20d8214bb62d2334010e978
Anthonin Bonnefoy <bonnefoa@poste.isima.fr>
2009-12-02 20:31:08 +0100

    Refactoring of tests

---
+++ izpack-test/src/test/java/com/izforge/izpack/integration/AbstractInstallationTest.java
@@ -0,0 +1,111 @@
+package com.izforge.izpack.integration;
+
+import com.izforge.izpack.AssertionHelper;
+import com.izforge.izpack.bootstrap.IApplicationComponent;
+import com.izforge.izpack.bootstrap.IPanelComponent;
+import com.izforge.izpack.compiler.CompilerConfig;
+import com.izforge.izpack.data.ResourceManager;
+import com.izforge.izpack.installer.base.InstallerFrame;
+import com.izforge.izpack.installer.base.LanguageDialog;
+import org.apache.commons.io.FileUtils;
+import org.fest.swing.fixture.DialogFixture;
+import org.fest.swing.fixture.FrameFixture;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.MethodRule;
+import org.junit.rules.Timeout;
+
+import java.io.File;
+
+/**
+ * Abstract test for integration test
+ */
+public class AbstractInstallationTest {
+    private File currentDir = new File(getClass().getClassLoader().getResource(".").getFile());
+
+    protected static final String APPNAME = "Test Installation";
+
+    @Rule
+    public MethodRule globalTimeout = new Timeout(60000);
+    protected IApplicationComponent applicationComponent;
+    protected IPanelComponent panelComponent;
+
+    protected FrameFixture installerFrameFixture;
+    protected DialogFixture dialogFrameFixture;
+
+
+    @Before
+    public void deleteLock() {
+        File file = new File(System.getProperty("java.io.tmpdir"), "iz-" + LanguageSelectionTest.APPNAME + ".tmp");
+        file.delete();
+    }
+
+    /**
+     * Prepare fest fixture for installer frame
+     *
+     * @throws Exception
+     */
+    protected void prepareFrameFixture() throws Exception {
+        InstallerFrame installerFrame = panelComponent.getComponent(InstallerFrame.class);
+        installerFrame.loadPanels();
+        installerFrameFixture = new FrameFixture(installerFrame);
+        installerFrameFixture.show();
+    }
+
+    /**
+     * Prepare fest fixture for lang selection
+     */
+    protected void prepareDialogFixture() {
+        LanguageDialog languageDialog = panelComponent.getComponent(LanguageDialog.class);
+        dialogFrameFixture = new DialogFixture(languageDialog);
+        dialogFrameFixture.show();
+    }
+
+    /**
+     * Compile an installer and unzip the created jar.
+     *
+     * @param installationFile The izpack installation file
+     * @param workingDirectory
+     * @throws Exception
+     */
+    protected void compileAndUnzip(String installationFile, File workingDirectory) throws Exception {
+        File installerFile = new File(workingDirectory, installationFile);
+        File out = new File(workingDirectory, "out.jar");
+        compileAndUnzip(installerFile, workingDirectory, out);
+    }
+
+    protected File getWorkingDirectory(String workingDirectoryName) {
+        File workingDirectory = new File(getClass().getClassLoader().getResource(workingDirectoryName).getFile());
+        setResourcePath(workingDirectory);
+        return workingDirectory;
+    }
+
+    /**
+     * Compile an installer and unzip it.
+     *
+     * @param installerFile    The izpack installer file
+     * @param workingDirectory The directory containing the installer file
+     * @param out              The output of the compiler
+     * @throws Exception
+     */
+    private void compileAndUnzip(File installerFile, File workingDirectory, File out) throws Exception {
+        CompilerConfig c = new CompilerConfig(installerFile.getAbsolutePath(), workingDirectory.getAbsolutePath(), "default", out.getAbsolutePath());
+        c.executeCompiler();
+        File extractedDir = new File(workingDirectory, "temp");
+        // Clean before use
+        FileUtils.deleteDirectory(extractedDir);
+        extractedDir.mkdirs();
+        AssertionHelper.unzipJar(out, extractedDir);
+    }
+
+    /**
+     * Set resource path in resource manager
+     *
+     * @param baseDir Base path where unzip has been operated
+     */
+    protected void setResourcePath(File baseDir) {
+        String relativePath = baseDir.getAbsolutePath().substring(currentDir.getAbsolutePath().length());
+        System.out.println(relativePath);
+        applicationComponent.getComponent(ResourceManager.class).setResourceBasePath(relativePath + "/temp/resources/");
+    }
+}
---
+++ izpack-test/src/test/java/com/izforge/izpack/integration/LanguageSelectionTest.java
@@ -0,0 +1,81 @@
+package com.izforge.izpack.integration;
+
+import com.izforge.izpack.bootstrap.ApplicationComponent;
+import com.izforge.izpack.bootstrap.IPanelComponent;
+import com.izforge.izpack.data.ResourceManager;
+import com.izforge.izpack.installer.base.GuiId;
+import com.izforge.izpack.installer.base.LanguageDialog;
+import org.fest.swing.exception.ScreenLockException;
+import org.hamcrest.core.Is;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Test for an installation using mock data
+ */
+public class LanguageSelectionTest extends AbstractInstallationTest {
+    @Before
+    public void initBinding() throws Throwable {
+        applicationComponent = new ApplicationComponent();
+        applicationComponent.initBindings();
+    }
+
+    @After
+    public void tearBinding() {
+        applicationComponent.dispose();
+        try {
+            if (dialogFrameFixture != null) {
+                dialogFrameFixture.cleanUp();
+                dialogFrameFixture = null;
+            }
+        } catch (ScreenLockException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void langpackEngShouldBeSet() throws Exception {
+        compileAndUnzip("engInstaller.xml", getWorkingDirectory("langpack"));
+        panelComponent = applicationComponent.getComponent(IPanelComponent.class);
+        panelComponent.getComponent(LanguageDialog.class).initLangPack();
+        ResourceManager resourceManager = applicationComponent.getComponent(ResourceManager.class);
+        assertThat(resourceManager.getLocale(), Is.is("eng"));
+    }
+
+    @Test
+    public void langpackFraShouldBeSet() throws Exception {
+        compileAndUnzip("fraInstaller.xml", getWorkingDirectory("langpack"));
+        panelComponent = applicationComponent.getComponent(IPanelComponent.class);
+        panelComponent.getComponent(LanguageDialog.class).initLangPack();
+        ResourceManager resourceManager = applicationComponent.getComponent(ResourceManager.class);
+        assertThat(resourceManager.getLocale(), Is.is("fra"));
+    }
+
+    @Test
+    public void testLangPickerChoseEng() throws Exception {
+        compileAndUnzip("basicInstall.xml", getWorkingDirectory("samples/basicInstall"));
+        panelComponent = applicationComponent.getComponent(IPanelComponent.class);
+        prepareDialogFixture();
+        assertThat(dialogFrameFixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).contents(), Is.is(new String[]{"eng", "fra"}));
+        dialogFrameFixture.button(GuiId.BUTTON_LANG_OK.id).click();
+        ResourceManager resourceManager = applicationComponent.getComponent(ResourceManager.class);
+        assertThat(resourceManager.getLocale(), Is.is("eng"));
+    }
+
+    @Test
+    public void testLangPickerChoseFra() throws Exception {
+        compileAndUnzip("basicInstall.xml", getWorkingDirectory("samples/basicInstall"));
+        panelComponent = applicationComponent.getComponent(IPanelComponent.class);
+        prepareDialogFixture();
+        assertThat(dialogFrameFixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).contents(), Is.is(new String[]{"eng", "fra"}));
+        dialogFrameFixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).selectItem(1);
+        dialogFrameFixture.button(GuiId.BUTTON_LANG_OK.id).click();
+        ResourceManager resourceManager = applicationComponent.getComponent(ResourceManager.class);
+        assertThat(resourceManager.getLocale(), Is.is("fra"));
+    }
+
+
+}
