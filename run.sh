18c2756781c7cfea67f832844332c9030e5c57c3
Anthonin Bonnefoy <bonnefoa@poste.isima.fr>
2009-12-05 23:02:35 +0100

    Add a scala test module for further tests in scala

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #/usr/local/bin/xvfb-run -s "-screen 0 1280x1024x24" /opt/maven/apache-maven-2.2.0/bin/mvn clean install
-XVFB_DISPLAY=:66
-XAUTH_FILE=/tmp/xauth-izpack
-killall Xvfb
-rm $XAUTH_FILE
-touch /tmp/xauth-izpack
-Xvfb $XVFB_DISPLAY -auth $XAUTH_FILE &
-sleep 5
-PID=pgrep Xvfb
-DISPLAY=$XVFB_DISPLAY
-/opt/maven/apache-maven-2.2.0/bin/mvn clean install
-kill $PID
-rm $XAUTH_FILE
+/usr/local/bin/xvfb-run -s "-screen 0 1280x1024x24" mvn clean install
+#XVFB_DISPLAY=:66
+#XAUTH_FILE=/tmp/xauth-izpack
+#killall Xvfb
+#rm $XAUTH_FILE
+#touch /tmp/xauth-izpack
+#Xvfb $XVFB_DISPLAY -auth $XAUTH_FILE &
+#sleep 5
+#PID=pgrep Xvfb
+#DISPLAY=$XVFB_DISPLAY
+#/opt/maven/apache-maven-2.2.0/bin/mvn clean install
+#kill $PID
+#rm $XAUTH_FILE
