2f3f7296293ae238eba0b8913ac3c0a0920facfd
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2009-12-08 23:40:41 +0100

    IZ-9: Clean on substitutor interface

--- izpack-api/src/main/java/com/izforge/izpack/util/VariableSubstitutor.java
+++ izpack-api/src/main/java/com/izforge/izpack/util/VariableSubstitutor.java
@@ -3,11 +3,7 @@ package com.izforge.izpack.util;
 import java.io.*;
 
 /**
- * Created by IntelliJ IDEA.
- * User: sora
- * Date: Nov 30, 2009
- * Time: 8:09:57 PM
- * To change this template use File | Settings | File Templates.
+ * Interface of the variable substitutor
  */
 public interface VariableSubstitutor extends Serializable {
     boolean areBracesRequired();
@@ -17,13 +13,9 @@ public interface VariableSubstitutor extends Serializable {
     String substitute(String str, String type) throws IllegalArgumentException;
 
     int substitute(InputStream in, OutputStream out, String type, String encoding)
-            throws IllegalArgumentException, UnsupportedEncodingException, IOException;
+            throws IllegalArgumentException, IOException;
 
-    String substitute(InputStream in, String type
-            )
-                    throws IllegalArgumentException, UnsupportedEncodingException,
-                    IOException;
+    String substitute(InputStream in, String type) throws IllegalArgumentException, IOException;
 
-    int substitute(Reader reader, Writer writer, String type)
-                            throws IllegalArgumentException, IOException;
+    int substitute(Reader reader, Writer writer, String type) throws IllegalArgumentException, IOException;
 }
