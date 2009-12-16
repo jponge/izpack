694ef7ed49d2193f2f497dcec1c22ab33f4dd6aa
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2009-12-16 20:29:23 +0100

    IZ-32: Bootstrap compiler with pico : Moved package

--- izpack-compiler/src/main/java/com/izforge/izpack/compiler/container/CompilerContainer.java
+++ izpack-compiler/src/main/java/com/izforge/izpack/compiler/container/CompilerContainer.java
@@ -1,16 +1,17 @@
-package com.izforge.izpack.compiler.bootstrap;
+package com.izforge.izpack.compiler.container;
 
 import com.izforge.izpack.container.AbstractContainer;
 import org.picocontainer.PicoBuilder;
 
 /**
  * Container for compiler
+ *
+ * @author Anthonin Bonnefoy
  */
 public class CompilerContainer extends AbstractContainer {
 
     public void initBindings() {
         pico = new PicoBuilder().withConstructorInjection().withCaching().build();
-
     }
 
 }
