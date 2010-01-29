bcbdb646ee29e9a124789b67b35b45a5447be530
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-01-29 07:40:07 +0100

    Simplified langpick test

--- izpack-test/src/test/java/com/izforge/izpack/integration/LanguageSelectionTest.java
+++ izpack-test/src/test/java/com/izforge/izpack/integration/LanguageSelectionTest.java
@@ -30,32 +30,12 @@ public class LanguageSelectionTest extends AbstractInstallationTest {
     }
 
     @Test
-    public void langpackEngShouldBeSet() throws Exception {
-        compileAndUnzip("engInstaller.xml", getWorkingDirectory("samples"));
-        installerContainer = applicationContainer.getComponent(IInstallerContainer.class);
-        installerContainer.getComponent(LanguageDialog.class).initLangPack();
-        ResourceManager resourceManager = applicationContainer.getComponent(ResourceManager.class);
-        assertThat(resourceManager.getLocale(), Is.is("eng"));
-    }
-
-    @Test
     public void langpackFraShouldBeSet() throws Exception {
         compileAndUnzip("fraInstaller.xml", getWorkingDirectory("samples"));
         installerContainer = applicationContainer.getComponent(IInstallerContainer.class);
         installerContainer.getComponent(LanguageDialog.class).initLangPack();
         ResourceManager resourceManager = applicationContainer.getComponent(ResourceManager.class);
         assertThat(resourceManager.getLocale(), Is.is("fra"));
-    }
-
-    @Test
-    public void testLangPickerChoseEng() throws Exception {
-        compileAndUnzip("basicInstall.xml", getWorkingDirectory("samples/basicInstall"));
-        installerContainer = applicationContainer.getComponent(IInstallerContainer.class);
-        dialogFrameFixture = prepareDialogFixture();
-        assertThat(dialogFrameFixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).contents(), Is.is(new String[]{"eng", "fra"}));
-        dialogFrameFixture.button(GuiId.BUTTON_LANG_OK.id).click();
-        ResourceManager resourceManager = applicationContainer.getComponent(ResourceManager.class);
-        assertThat(resourceManager.getLocale(), Is.is("eng"));
     }
 
     @Test
