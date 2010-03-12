437de3863376e18b7385a684d433365b9af8263c
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-03-12 22:33:24 +0100

    Created Bindeable container

---
+++ izpack-api/src/main/java/com/izforge/izpack/api/container/BindeableContainer.java
@@ -0,0 +1,21 @@
+package com.izforge.izpack.api.container;
+
+/**
+ * Interface for applcation container using Pico
+ *
+ * @author Anthonin Bonnefoy
+ */
+public interface BindeableContainer
+{
+    <T> void addComponent(Class<T> componentType);
+
+    <T> T getComponent(final Class<T> componentType);
+
+    void addComponent(Object componentType, Object implementation);
+
+    Object getComponent(Object componentKeyOrType);
+
+    void initBindings();
+
+    void dispose();
+}
