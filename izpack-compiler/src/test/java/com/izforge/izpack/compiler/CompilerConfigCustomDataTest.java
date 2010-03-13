301d062aab9e8ae057247bc9fc7f199de24d2f1a
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-03-13 23:28:44 +0100

    Rewrote custom data tests using PicoRunner

--- izpack-compiler/src/test/java/com/izforge/izpack/compiler/CompilerConfigCustomDataTest.java
+++ izpack-compiler/src/test/java/com/izforge/izpack/compiler/CompilerConfigCustomDataTest.java
@@ -1,16 +1,24 @@
 package com.izforge.izpack.compiler;
 
+import com.izforge.izpack.api.container.BindeableContainer;
 import com.izforge.izpack.api.data.binding.IzpackProjectInstaller;
 import com.izforge.izpack.api.data.binding.Listener;
 import com.izforge.izpack.api.data.binding.Stage;
+import com.izforge.izpack.compiler.container.TestCompilerContainer;
 import com.izforge.izpack.matcher.ObjectInputMatcher;
 import com.izforge.izpack.matcher.ZipMatcher;
+import com.izforge.izpack.test.BaseDir;
+import com.izforge.izpack.test.Container;
+import com.izforge.izpack.test.InstallFile;
+import com.izforge.izpack.test.junit.PicoRunner;
 import org.hamcrest.beans.HasPropertyWithValue;
 import org.hamcrest.core.AllOf;
 import org.hamcrest.core.Is;
+import org.junit.Test;
 import org.junit.internal.matchers.IsCollectionContaining;
-import org.testng.annotations.Test;
+import org.junit.runner.RunWith;
 
+import java.io.File;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,11 +28,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
  *
  * @author Anthonin Bonnefoy
  */
-public class CustomDataTest extends AbstractCompilationTest
+@RunWith(PicoRunner.class)
+@Container(TestCompilerContainer.class)
+@BaseDir("samples")
+@InstallFile("samples/listeners.xml")
+public class CompilerConfigCustomDataTest
 {
-    String getInstallFileName()
+    private File out;
+    private BindeableContainer compilerContainer;
+
+    public CompilerConfigCustomDataTest(File out, BindeableContainer compilerContainer)
     {
-        return "samples/listeners.xml";
+        this.out = out;
+        this.compilerContainer = compilerContainer;
     }
 
     @Test
@@ -45,8 +61,7 @@ public class CustomDataTest extends AbstractCompilationTest
                         HasPropertyWithValue.<Listener>hasProperty("classname", Is.is("RegistryInstallerListener")),
                         HasPropertyWithValue.<Listener>hasProperty("stage", Is.is(Stage.install))
                 )
-        )
-        );
+        ));
 
     }
 
