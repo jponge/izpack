b1501d4e9ac804d1c27d6c2ce356b2a472f3879f
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-03-14 11:33:34 +0100

    Extracted ClassUtils for jar load manipulation

--- izpack-test/src/test/java/com/izforge/izpack/integration/AbstractIntegrationTest.java
+++ izpack-test/src/test/java/com/izforge/izpack/integration/AbstractIntegrationTest.java
@@ -13,17 +13,9 @@ import org.fest.swing.fixture.DialogFixture;
 import org.fest.swing.fixture.FrameFixture;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import sun.misc.URLClassPath;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
 
 /**
  * Abstract test for integration test
@@ -129,35 +121,14 @@ public class AbstractIntegrationTest
         compilerContainer.addComponent(CompilerData.class, data);
         CompilerConfig compilerConfig = compilerContainer.getComponent(CompilerConfig.class);
         compilerConfig.executeCompiler();
-        loadJarInSystemClassLoader(out);
+        ClassUtils.loadJarInSystemClassLoader(out);
         initInstallerContainer();
     }
 
     @AfterMethod
     public void unloadJarInSystemClassLoader() throws Exception
     {
-        URLClassLoader systemClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-        Field ucpField = URLClassLoader.class.getDeclaredField("ucp");
-        ucpField.setAccessible(true);
-        URLClassPath ucp = (URLClassPath) ucpField.get(systemClassLoader);
-        Field pathField = URLClassPath.class.getDeclaredField("path");
-        pathField.setAccessible(true);
-        ArrayList<URL> path = (ArrayList) pathField.get(ucp);
-        path.remove(path.size() - 1);
-
-        Field loaderField = URLClassPath.class.getDeclaredField("loaders");
-        loaderField.setAccessible(true);
-        ArrayList loaders = (ArrayList) loaderField.get(ucp);
-        loaders.remove(loaders.size() - 1);
-    }
-
-    private void loadJarInSystemClassLoader(File out) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, MalformedURLException
-    {
-        URLClassLoader systemClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-
-        Method declaredMethod = URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{URL.class});
-        declaredMethod.setAccessible(true);
-        declaredMethod.invoke(systemClassLoader, out.toURI().toURL());
+        ClassUtils.unloadLastJar();
     }
 
 }
