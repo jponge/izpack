7221b8a2d73098f5cc5b661df451a759a3a8a994
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-04-01 21:26:17 +0200

    Tests for condition check

---
+++ izpack-test/src/test/java/com/izforge/izpack/installer/language/ConditionCheckTest.java
@@ -0,0 +1,40 @@
+package com.izforge.izpack.installer.language;
+
+import com.izforge.izpack.api.installer.InstallerRequirementDisplay;
+import com.izforge.izpack.compiler.container.TestIntegrationContainer;
+import com.izforge.izpack.test.Container;
+import com.izforge.izpack.test.InstallFile;
+import com.izforge.izpack.test.junit.PicoRunner;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Test installerRequirements
+ *
+ * @author Anthonin Bonnefoy
+ */
+@RunWith(PicoRunner.class)
+@Container(TestIntegrationContainer.class)
+public class ConditionCheckTest
+{
+
+    private ConditionCheck conditionCheck;
+
+    public ConditionCheckTest(ConditionCheck conditionCheck)
+    {
+        this.conditionCheck = conditionCheck;
+    }
+
+    @Test
+    @InstallFile("samples/checkRequirement.xml")
+    public void testCheckInstallerRequirements() throws Exception
+    {
+        InstallerRequirementDisplay requirementDisplay = Mockito.mock(InstallerRequirementDisplay.class);
+        assertThat(conditionCheck.checkInstallerRequirements(requirementDisplay), Is.is(false));
+        Mockito.verify(requirementDisplay).showMissingRequirementMessage("42");
+    }
+}
