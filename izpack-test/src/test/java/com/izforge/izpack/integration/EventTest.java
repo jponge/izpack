0497aa05c6f5ec70bb18979d82e30df59d6c9140
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-05-14 19:50:04 +0200

    Created test for event initialization problems

---
+++ izpack-test/src/test/java/com/izforge/izpack/integration/EventTest.java
@@ -0,0 +1,26 @@
+package com.izforge.izpack.integration;
+
+import com.izforge.izpack.compiler.container.TestIntegrationContainer;
+import com.izforge.izpack.test.Container;
+import com.izforge.izpack.test.InstallFile;
+import com.izforge.izpack.test.junit.PicoRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test for event binding
+ *
+ * @author Anthonin Bonnefoy
+ */
+
+@RunWith(PicoRunner.class)
+@Container(TestIntegrationContainer.class)
+public class EventTest
+{
+    @Test
+    @InstallFile("samples/event/event.xml")
+    public void eventInitialization() throws Exception
+    {
+
+    }
+}
