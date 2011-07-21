22b55da6e3f789f1fd42e883945432853d41cd97
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-07-21 09:53:19 +0200

    Enable console mode and automatic installation mode for the extended install panel

---
+++ src/lib/com/izforge/izpack/panels/ExtendedInstallPanelAutomationHelper.java
@@ -0,0 +1,43 @@
+/**
+ * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
+ * Copyright (C) 2002-2011 Jahia Solutions Group SA. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have received a copy of the text
+ * describing the FLOSS exception, and it is also available here:
+ * http://www.jahia.com/license
+ *
+ * Commercial and Supported Versions of the program
+ * Alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms contained in a separate written agreement
+ * between you and Jahia Solutions Group SA. If you are unsure which license is appropriate
+ * for your use, please contact the sales department at sales@jahia.com.
+ */
+
+package com.izforge.izpack.panels;
+
+
+/**
+ * Automation helper for the Extended Install Panel.
+ *
+ * @author Sergiy Shyrkov
+ */
+public class ExtendedInstallPanelAutomationHelper extends InstallPanelAutomationHelper
+{
+}
---
+++ src/lib/com/izforge/izpack/panels/ExtendedInstallPanelConsoleHelper.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2002-2008 Jahia Ltd
+ *
+ * Licensed under the JAHIA COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (JCDDL), 
+ * Version 1.0 (the "License"), or (at your option) any later version; you may 
+ * not use this file except in compliance with the License. You should have 
+ * received a copy of the License along with this program; if not, you may obtain 
+ * a copy of the License at 
+ *
+ *  http://www.jahia.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.izforge.izpack.panels;
+
+
+/**
+ * Extended Install Panel console helper.
+ *
+ * @author Sergiy Shyrkov
+ */
+public class ExtendedInstallPanelConsoleHelper extends InstallPanelConsoleHelper
+{
+}
