09aeae327ac2ab41c8687fb337728caba1772658
dantran <dantran@gmail.com>
2011-09-01 19:00:46 -0700

    [IZPACK-689] Serialize Action to turn off exception during panel action compilation

--- izpack-api/src/main/java/com/izforge/izpack/api/data/binding/Action.java
+++ izpack-api/src/main/java/com/izforge/izpack/api/data/binding/Action.java
@@ -1,12 +1,16 @@
 package com.izforge.izpack.api.data.binding;
 
+import java.io.Serializable;
+
 /**
  * Action for panel
  *
  * @author Anthonin Bonnefoy
  */
-public class Action
+public class Action implements Serializable
 {
+    private static final long serialVersionUID = 1L;
+
     private String classname;
 
     private ActionStage actionStage;
