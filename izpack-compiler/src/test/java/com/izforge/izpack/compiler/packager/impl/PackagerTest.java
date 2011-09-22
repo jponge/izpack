0f37fa272153f15bf2f48afd23dd28ef5738216c
Mykola Nikishov <mn@mn.com.ua>
2011-09-22 20:43:45 +0300

    Mock ResourceFinder

--- izpack-compiler/src/test/java/com/izforge/izpack/compiler/packager/impl/PackagerTest.java
+++ izpack-compiler/src/test/java/com/izforge/izpack/compiler/packager/impl/PackagerTest.java
@@ -3,13 +3,13 @@ package com.izforge.izpack.compiler.packager.impl;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
 import org.dom4j.dom.DOMElement;
 import org.junit.Test;
 
-import com.izforge.izpack.api.adaptator.IXMLElement;
 import com.izforge.izpack.api.adaptator.impl.XMLElementImpl;
 import com.izforge.izpack.compiler.resource.ResourceFinder;
 import com.izforge.izpack.merge.MergeManager;
@@ -18,16 +18,13 @@ public class PackagerTest {
 
 	@Test
 	public void guiPrefsWithNoSplash() throws IOException {
-		final ResourceFinder resourceFinder = new ResourceFinder(null, null,
-				null, null) {
-			@Override
-			public IXMLElement getXMLTree() throws IOException {
-				final DOMElement rootNode = new DOMElement("installation");
-				final DOMElement guiPrefsNode = new DOMElement("guiprefs");
-				rootNode.add(guiPrefsNode);
-				return new XMLElementImpl(rootNode);
-			}
-		};
+		final ResourceFinder resourceFinder = mock(ResourceFinder.class);
+
+		final DOMElement rootNode = new DOMElement("installation");
+		rootNode.add(new DOMElement("guiprefs"));
+		when(resourceFinder.getXMLTree()).thenReturn(
+				new XMLElementImpl(rootNode));
+
 		final MergeManager mockMergeManager = mock(MergeManager.class);
 
 		final Packager packager = new Packager(null, null, null, null, null,
@@ -41,17 +38,14 @@ public class PackagerTest {
 
 	@Test
 	public void guiPrefsWithSplash() throws IOException {
-		final ResourceFinder resourceFinder = new ResourceFinder(null, null,
-				null, null) {
-			@Override
-			public IXMLElement getXMLTree() throws IOException {
-				final DOMElement rootNode = new DOMElement("installation");
-				final DOMElement guiPrefsNode = new DOMElement("guiprefs");
-				guiPrefsNode.add(new DOMElement("splash"));
-				rootNode.add(guiPrefsNode);
-				return new XMLElementImpl(rootNode);
-			}
-		};
+		final ResourceFinder resourceFinder = mock(ResourceFinder.class);
+		final DOMElement rootNode = new DOMElement("installation");
+		final DOMElement guiPrefsNode = new DOMElement("guiprefs");
+		guiPrefsNode.add(new DOMElement("splash"));
+		rootNode.add(guiPrefsNode);
+		when(resourceFinder.getXMLTree()).thenReturn(
+				new XMLElementImpl(rootNode));
+
 		final MergeManager mockMergeManager = mock(MergeManager.class);
 
 		final Packager packager = new Packager(null, null, null, null, null,
@@ -65,13 +59,10 @@ public class PackagerTest {
 
 	@Test
 	public void noGuiPrefs() throws IOException {
-		final ResourceFinder resourceFinder = new ResourceFinder(null, null,
-				null, null) {
-			@Override
-			public IXMLElement getXMLTree() throws IOException {
-				return new XMLElementImpl(new DOMElement("installation"));
-			}
-		};
+		final ResourceFinder resourceFinder = mock(ResourceFinder.class);
+		when(resourceFinder.getXMLTree()).thenReturn(
+				new XMLElementImpl(new DOMElement("installation")));
+
 		final MergeManager mockMergeManager = mock(MergeManager.class);
 
 		final Packager packager = new Packager(null, null, null, null, null,
