546cdfc34b9ff407cae87a1c1bdbf471bff1ec67
Mykola Nikishov <mn@mn.com.ua>
2011-09-23 03:31:10 +0300

    Verify number of calls to MergeManager
    If we have a splash image, there should be two calls to
    com.izforge.izpack.merge.MergeManager's addResourceToMerge(String,
    String).

--- izpack-compiler/src/test/java/com/izforge/izpack/compiler/packager/impl/PackagerTest.java
+++ izpack-compiler/src/test/java/com/izforge/izpack/compiler/packager/impl/PackagerTest.java
@@ -2,21 +2,24 @@ package com.izforge.izpack.compiler.packager.impl;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.dom4j.dom.DOMElement;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.izforge.izpack.api.adaptator.impl.XMLElementImpl;
 import com.izforge.izpack.compiler.resource.ResourceFinder;
 import com.izforge.izpack.merge.MergeManager;
-import com.izforge.izpack.util.file.FileUtils;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(FileUtils.class)
@@ -44,6 +47,10 @@ public class PackagerTest {
 
 	@Test
 	public void guiPrefsWithSplash() throws IOException {
+		PowerMockito.mockStatic(FileUtils.class);
+		final File splashImage = new File("");
+		when(FileUtils.toFile(null)).thenReturn(splashImage);
+
 		final ResourceFinder resourceFinder = mock(ResourceFinder.class);
 		final DOMElement rootNode = new DOMElement("installation");
 		final DOMElement guiPrefsNode = new DOMElement("guiprefs");
@@ -59,8 +66,8 @@ public class PackagerTest {
 
 		packager.writeManifest();
 
-		verify(mockMergeManager).addResourceToMerge(anyString(), anyString());
-
+		verify(mockMergeManager, times(2)).addResourceToMerge(anyString(),
+				anyString());
 	}
 
 	@Test
