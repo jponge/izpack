f4afaad6229d9042f287e9f006c71faeb050ab28
Julien Ponge <julien.ponge@gmail.com>
2011-11-26 19:37:46 +0100

    Version bump

--- src/lib/com/izforge/izpack/compiler/Compiler.java
+++ src/lib/com/izforge/izpack/compiler/Compiler.java
@@ -56,7 +56,7 @@ public class Compiler extends Thread
     /**
      * The IzPack version.
      */
-    public final static String IZPACK_VERSION = "4.3.4";
+    public final static String IZPACK_VERSION = "4.3.5";
 
     /**
      * The IzPack home directory.
