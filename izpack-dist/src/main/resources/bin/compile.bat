b0c7763dce4afad273663055a822dca0193a0457
Julien Ponge <julien.ponge@gmail.com>
2012-04-23 21:45:24 +0200

    Fix for IZPACK-687

--- izpack-dist/src/main/resources/bin/compile.bat
+++ izpack-dist/src/main/resources/bin/compile.bat
@@ -23,6 +23,8 @@ set DEFAULT_IZPACK_HOME=%~dp0..
 if "%IZPACK_HOME%"=="" set IZPACK_HOME=%DEFAULT_IZPACK_HOME%
 set DEFAULT_IZPACK_HOME=
 
+echo IZPACK_HOME is "%IZPACK_HOME%"
+
 rem Slurp the command line arguments. This loop allows for an unlimited number
 rem of arguments (up to the command line limit, anyway).
 set IZPACK_CMD_LINE_ARGS=%1
@@ -40,7 +42,7 @@ rem and for NT handling to skip to.
 rem find IZPACK_HOME if it does not exist due to either
 rem an invalid value passed by the user or the %0 problem 
 rem on Windows 9x
-set IZPACK_JAR=lib\standalone-compiler.jar
+set IZPACK_JAR=lib\izpack-compiler-*.jar
 if exist "%IZPACK_HOME%\%IZPACK_JAR%" goto checkJava
 
 rem check for Izpack in Program Files
