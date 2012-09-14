72e5eded40f796183aab66a9cd7dc2f6ff18c3e4
Tim Anderson <tma@netspace.net.au>
2012-09-14 23:43:38 +1000

    Fix for IZPACK-857 ResourceNotFoundException: Image icon resource not found in flag.English

---
+++ izpack-installer/src/main/java/com/izforge/izpack/installer/language/Languages.java
@@ -0,0 +1,207 @@
+/*
+ * IzPack - Copyright 2001-2012 Julien Ponge, All Rights Reserved.
+ *
+ * http://izpack.org/
+ * http://izpack.codehaus.org/
+ *
+ * Copyright 2012 Tim Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izforge.izpack.installer.language;
+
+import java.awt.Font;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import com.izforge.izpack.api.resource.Locales;
+import com.izforge.izpack.installer.data.GUIInstallData;
+
+
+/**
+ * Helper to map ISO3 codes to their corresponding display names according to the configured "langDisplayType"
+ * modifier.
+ *
+ * @author Tim Anderson
+ */
+class Languages
+{
+
+    enum DisplayType
+    {
+        ISO3,     //  indicates to display ISO3 language codes
+        NATIVE,   // indicates to display the native name for a language.
+        DEFAULT   // Indicates to display the default name for a language.
+    }
+
+    /**
+     * The display type. One of "iso3", "native" or "default".
+     */
+    private DisplayType displayType;
+
+    /**
+     * The language display names, keyed on their ISO3 codes.
+     */
+    private final Map<String, String> displayNames = new LinkedHashMap<String, String>();
+
+    /**
+     * The logger.
+     */
+    private static final Logger logger = Logger.getLogger(Languages.class.getName());
+
+
+    /**
+     * Constructs a {@code Languages}.
+     *
+     * @param locales     the locales
+     * @param installData the installation data
+     * @param font        the font to verify that language display names can be displayed. May be {@code null}
+     */
+    public Languages(Locales locales, GUIInstallData installData, Font font)
+    {
+        displayType = getDisplayType(installData);
+        if (displayType == DisplayType.NATIVE && font == null)
+        {
+            logger.info("Cannot render native language display names - no font supplied for verification");
+            displayType = DisplayType.DEFAULT;
+        }
+
+        DisplayNameCollector collector;
+        if (displayType == DisplayType.NATIVE)
+        {
+            collector = new NativeDisplayNameCollector(font);
+        }
+        else if (displayType == DisplayType.DEFAULT)
+        {
+            collector = new DefaultDisplayNameCollector();
+        }
+        else
+        {
+            collector = new ISO3CodeCollector();
+        }
+        for (Locale locale : locales.getLocales())
+        {
+            collector.addDisplayName(locale, displayNames);
+        }
+    }
+
+    /**
+     * Returns the type in which the languages will be displayed.
+     *
+     * @return the display type
+     */
+    public DisplayType getDisplayType()
+    {
+        return displayType;
+    }
+
+    /**
+     * Returns the language display names, keyed on their ISO3 codes.
+     *
+     * @return the language display names
+     */
+    public Map<String, String> getDisplayNames()
+    {
+        return displayNames;
+    }
+
+    /**
+     * Returns the type in which the language should be displayed.
+     *
+     * @return the language display type
+     */
+    private DisplayType getDisplayType(GUIInstallData installData)
+    {
+        DisplayType result = DisplayType.DEFAULT;
+        Map<String, String> modifier = installData.guiPrefs.modifier;
+        String langDisplayType = modifier.get("langDisplayType");
+        if (langDisplayType != null && langDisplayType.length() != 0)
+        {
+            try
+            {
+                result = DisplayType.valueOf(langDisplayType.toUpperCase());
+            }
+            catch (IllegalArgumentException exception)
+            {
+                logger.warning("Invalid langDisplayType: " + langDisplayType);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Collects display names.
+     */
+    private interface DisplayNameCollector
+    {
+        void addDisplayName(Locale locale, Map<String, String> displayNames);
+    }
+
+    /**
+     * Collects ISO3 codes.
+     */
+    private class ISO3CodeCollector implements DisplayNameCollector
+    {
+
+        @Override
+        public void addDisplayName(Locale locale, Map<String, String> displayNames)
+        {
+            String code = locale.getISO3Language();
+            displayNames.put(code, code);
+        }
+    }
+
+    /**
+     * Collects display names as they are written in the default locale.
+     */
+    private class DefaultDisplayNameCollector implements DisplayNameCollector
+    {
+        @Override
+        public void addDisplayName(Locale locale, Map<String, String> displayNames)
+        {
+            displayNames.put(locale.getISO3Language(), locale.getDisplayLanguage());
+        }
+    }
+
+    /**
+     * Collects display names in the language of the supplied locale. If the language is not displayable by the
+     * supplied font, the language name will be returned as written in the default locale.
+     */
+    private class NativeDisplayNameCollector implements DisplayNameCollector
+    {
+
+        private final Font font;
+
+        public NativeDisplayNameCollector(Font font)
+        {
+            this.font = font;
+        }
+
+        @Override
+        public void addDisplayName(Locale locale, Map<String, String> displayNames)
+        {
+            String name = locale.getDisplayLanguage(locale);
+            if (font.canDisplayUpTo(name) > -1)
+            {
+                // Font cannot render it; use language name in the spelling of the default locale.
+                name = locale.getDisplayLanguage();
+            }
+            displayNames.put(locale.getISO3Language(), name);
+        }
+
+    }
+
+}
--- izpack-installer/src/test/java/com/izforge/izpack/installer/language/LanguageDialogTest.java
+++ izpack-installer/src/test/java/com/izforge/izpack/installer/language/LanguageDialogTest.java
@@ -1,60 +1,176 @@
+/*
+ * IzPack - Copyright 2001-2012 Julien Ponge, All Rights Reserved.
+ *
+ * http://izpack.org/
+ * http://izpack.codehaus.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.izforge.izpack.installer.language;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Locale;
+
+import javax.swing.JFrame;
+
 import org.fest.swing.fixture.DialogFixture;
 import org.hamcrest.core.Is;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 import com.izforge.izpack.api.GuiId;
-import com.izforge.izpack.api.container.Container;
 import com.izforge.izpack.api.resource.Locales;
+import com.izforge.izpack.api.resource.Resources;
 import com.izforge.izpack.installer.container.TestLanguageContainer;
+import com.izforge.izpack.installer.data.GUIInstallData;
+import com.izforge.izpack.installer.requirement.RequirementsChecker;
+import com.izforge.izpack.test.Container;
 import com.izforge.izpack.test.junit.PicoRunner;
 
 /**
- * Test for an installation using mock data
+ * Tests the {@link LanguageDialog}.
+ *
+ * @author Anthonin Bonnefoy
+ * @author Tim Anderson
  */
 @RunWith(PicoRunner.class)
-@com.izforge.izpack.test.Container(TestLanguageContainer.class)
+@Container(TestLanguageContainer.class)
 public class LanguageDialogTest
 {
-    private Container installerContainer;
-    private DialogFixture dialogFixture;
-    private Locales locales;
+    /**
+     * The resources.
+     */
+    private final Resources resources;
 
-    public LanguageDialogTest(Container installerContainer, DialogFixture dialogFixture, Locales locales)
+    /**
+     * The installation data.
+     */
+    private final GUIInstallData installData;
+
+    /**
+     * The locales.
+     */
+    private final Locales locales;
+
+    /**
+     * The dialog fixture.
+     */
+    private DialogFixture fixture;
+
+    /**
+     * Constructs {@code LanguageDialogTest}.
+     *
+     * @param resources   the resources
+     * @param installData the installation data
+     * @param locales     the locales. Must contain locales "eng" and "fra"
+     */
+    public LanguageDialogTest(Resources resources, GUIInstallData installData, Locales locales)
     {
-        this.installerContainer = installerContainer;
-        this.dialogFixture = dialogFixture;
+        this.resources = resources;
+        this.installData = installData;
         this.locales = locales;
     }
 
+    /**
+     * Cleans up after the test.
+     */
     @After
     public void tearBinding()
     {
-        installerContainer.dispose();
-        if (dialogFixture != null)
+        if (fixture != null)
         {
-            dialogFixture.cleanUp();
-            dialogFixture = null;
+            fixture.cleanUp();
+            fixture = null;
         }
     }
 
+    /**
+     * Tests the "default" language display type.
+     */
     @Test
-    public void testLangPickerChoseFra() throws Exception
+    public void testDefaultDisplayType()
     {
-        dialogFixture.show();
-        assertThat(dialogFixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).contents(), Is.is(new String[]{"eng", "fra"}));
-        dialogFixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).selectItem(1);
-        dialogFixture.button(GuiId.BUTTON_LANG_OK.id).click();
+        fixture = new DialogFixture(createDialog("default"));
+
+        String eng = locales.getLocale("eng").getDisplayLanguage();
+        String fra = locales.getLocale("fra").getDisplayLanguage();
+
+        checkSelectLanguage(eng, fra);
+    }
+
+    /**
+     * Tests the "native" language display type.
+     */
+    @Test
+    public void testNativeDisplayType()
+    {
+        LanguageDialog dialog = createDialog("native");
+        fixture = new DialogFixture(dialog);
+
+        Locale engLocale = locales.getLocale("eng");
+        Locale fraLocale = locales.getLocale("fra");
+        String eng = engLocale.getDisplayLanguage(engLocale);
+        String fra = fraLocale.getDisplayLanguage(fraLocale);
+
+        checkSelectLanguage(eng, fra);
+    }
+
+    /**
+     * Tests the "iso3" language display type.
+     */
+    @Test
+    public void testISO3DisplayType()
+    {
+        fixture = new DialogFixture(createDialog("iso3"));
+        checkSelectLanguage("eng", "fra");
+    }
+
+    /**
+     * Verifies that the combo box has the correct elements for English and French and that the locale is set correctly
+     * when French is selected.
+     *
+     * @param englishDisplayName the expected display name for English
+     * @param frenchDisplayName  the expected display name for French
+     */
+    private void checkSelectLanguage(String englishDisplayName, String frenchDisplayName)
+    {
+        fixture.show();
+        assertThat(fixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).contents(),
+                   Is.is(new String[]{englishDisplayName, frenchDisplayName}));
+        fixture.comboBox(GuiId.COMBO_BOX_LANG_FLAG.id).selectItem(1);
+        fixture.button(GuiId.BUTTON_LANG_OK.id).click();
         assertNotNull(locales.getLocale());
         assertEquals("fra", locales.getLocale().getISO3Language());
     }
 
+    /**
+     * Creates a new {@link LanguageDialog} that displays language names according to the supplied {@code
+     * langDisplayType}.
+     *
+     * @param langDisplayType one of "native", "default", "iso3"
+     * @return a new dialog
+     */
+    private LanguageDialog createDialog(String langDisplayType)
+    {
+        installData.guiPrefs.modifier.put("langDisplayType", langDisplayType);
+        JFrame frame = new JFrame();
+        frame.setLocationRelativeTo(null);
+        return new LanguageDialog(frame, resources, locales, installData, Mockito.mock(RequirementsChecker.class));
+    }
 
 }
